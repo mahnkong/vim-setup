751bb254386e22bb6aa42df92c6a594581d36291
mahnkong <mahnkong@gmx.de>
2016-11-05 14:57:27 +0100

    more plugins

--- setup.sh
+++ setup.sh
@@ -5,7 +5,7 @@ curl -LSso $HOME/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
 
 cp $(dirname $0)/_vimrc $HOME/.vimrc
 
-for plugin in tpope/vim-endwise yegappan/mru scrooloose/syntastic ervandew/supertab Raimondi/delimitMate majutsushi/tagbar fatih/vim-go scrooloose/nerdtree jistr/vim-nerdtree-tabs vim-scripts/YankRing.vim fatih/vim-go.git Shougo/neocomplete.vim rodjek/vim-puppet.git; do
+for plugin in tpope/vim-endwise yegappan/mru scrooloose/syntastic ervandew/supertab Raimondi/delimitMate majutsushi/tagbar fatih/vim-go scrooloose/nerdtree jistr/vim-nerdtree-tabs vim-scripts/YankRing.vim fatih/vim-go.git Shougo/neocomplete.vim rodjek/vim-puppet.git tfnico/vim-gradle; do
     rm -rf $HOME/.vim/bundle/$(basename $plugin)
     git clone --depth 1 https://github.com/$plugin $HOME/.vim/bundle/$(basename $plugin)
 done
